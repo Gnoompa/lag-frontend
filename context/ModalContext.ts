90626a614033bb9d0fe6439d50ac411f2cf03558
Gnoompa <gnoompa@Gnoompas-MacBook-Pro.local>
2024-04-29 10:42:02 +0400

    removes garbage

--- context/ModalContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-interface IModalContext {
-    isOpen: boolean,
-    open: Function,
-    close: Function
-}
-
-export const ModalContext = createContext<IModalContext | undefined>(undefined);
